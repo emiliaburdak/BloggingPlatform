4377577753d542a2c85389ff7350bbc3acc682d5
Em <emiliaburdak@gmail.com>
2023-05-08 19:54:55 +0700

    funkcje : dodaj post, usun post, wyswietl wszytskie posty, wyswietl posty danego uzytkownika + zaczete juz tworzene komentarzy ale zrobione sa models

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
-db = SQLAlchemy
-DB_NAME = 'database.db'
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'mysecretkey'
+    app.config['SECRET_KEY'] = 'dupaldupal'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
@@ -17,6 +18,8 @@ def create_app():
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User, Post
 
     with app.app_context():
         db.create_all()
@@ -31,6 +34,7 @@ def create_app():
 
     return app
 
-def create_databese(app):
-    if not path.exists(('app/' +DB_NAME)):
+
+def create_detabase(app):
+    if not path.exists('app/' + DB_NAME):
         db.create_all()
--- app/views.py
+++ app/views.py
@@ -1,8 +1,64 @@
 from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
 from flask_login import login_required, current_user
-from flask.flask4.app.template.models import Note, Hashtag
-from flask.flask4.app.template import db
-import json
-from datetime import datetime
+from .models import Post, User
+from . import db
+
+# import json
+# from datetime import datetime
 
 views = Blueprint('views', __name__)
+
+
+@views.route('/')
+@views.route('/home')
+@login_required
+def home():
+    posts = Post.query.all()
+    return render_template('home.html', user=current_user, posts=posts)
+
+
+@views.route('/add_post', methods=['POST', 'GET'])
+@login_required
+def add_post():
+    if request.method == 'POST':
+        post = request.form.get('post')
+        if len(post) < 1:
+            flash('Post is too short!')
+        else:
+            new_post = Post(post=post, author=current_user.id)
+            db.session.add(new_post)
+            db.session.commit()
+            return redirect(url_for('views.home'))
+
+    return render_template('home.html', user=current_user)
+
+
+@views.route('/delete_post/<id>')
+@login_required
+def delete_post(id):
+    post = Post.query.filter_by(id=id).first()
+    if not post:
+        flash('Post does not exist', category='error')
+    elif current_user.id != post.author:
+        flash('You dont have permission', category='error')
+    else:
+        db.session.delete(post)
+        db.session.commit()
+        flash('Post deleted', category='success')
+
+    return redirect(url_for('views.home'))
+
+
+@views.route('/user_posts/<username>')
+@login_required
+def user_posts(username):
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        flash('User does not exist', category='error')
+        return redirect(url_for('views.home'))
+    else:
+        # posts = Post.query.filter_by(author=user.id).all() jeden sposób
+        posts = user.posts
+        return render_template('user_posts.html', user=user, posts=posts)
+
+
